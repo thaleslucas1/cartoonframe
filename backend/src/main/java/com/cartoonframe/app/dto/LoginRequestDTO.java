4a7865183e0b54ff8ff925e8f2c69a42fd2823e9
thales <thaleslucas36@gmail.com>
2025-07-01 11:27:34 -0300

    fix: parametros de dto esperados diferentes dos enviados pelo frontend

--- backend/src/main/java/com/cartoonframe/app/dto/LoginRequestDTO.java
+++ backend/src/main/java/com/cartoonframe/app/dto/LoginRequestDTO.java
@@ -1,4 +1,4 @@
 package com.cartoonframe.app.dto;
 
-public record LoginRequestDTO(String email, String password, String nickname){
+public record LoginRequestDTO(String identifier, String password){
 }
