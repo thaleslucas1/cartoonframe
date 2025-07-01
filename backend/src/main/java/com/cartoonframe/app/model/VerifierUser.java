8ff59f24820ddc358389fb8ffcc7b750470199a2
thales <thaleslucas36@gmail.com>
2025-07-01 02:38:23 -0300

    feat: criação de uma classe base para verificação de email

---
+++ backend/src/main/java/com/cartoonframe/app/model/VerifierUser.java
@@ -0,0 +1,56 @@
+package com.cartoonframe.app.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "VerifierUser")
+public class VerifierUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private Instant expirationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_USER", referencedColumnName = "ID", unique = true)
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public Instant getExpirationDate() {
+        return expirationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setExpirationDate(Instant expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
