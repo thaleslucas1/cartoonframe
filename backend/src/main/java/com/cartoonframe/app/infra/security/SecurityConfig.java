235c9fb97322ee5dbe3e803200481c3ae88fe773
thales <thaleslucas36@gmail.com>
2025-07-01 12:26:37 -0300

    feat: libera os endpoints de password reset

--- backend/src/main/java/com/cartoonframe/app/infra/security/SecurityConfig.java
+++ backend/src/main/java/com/cartoonframe/app/infra/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/auth/verify").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/password-reset/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
