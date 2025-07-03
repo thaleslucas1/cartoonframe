2faa046a21cc44ced2bfd279e64400a60ff294ce
thales <thaleslucas36@gmail.com>
2025-07-03 19:41:02 -0300

    feat: adição da busca das datas no repositorio das tentativas

--- backend/src/main/java/com/cartoonframe/app/repository/GuessesRepository.java
+++ backend/src/main/java/com/cartoonframe/app/repository/GuessesRepository.java
@@ -4,9 +4,11 @@ import com.cartoonframe.app.model.Guess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface GuessesRepository extends JpaRepository<Guess, Long> {
     List<Guess> findByChallenge_Id(long challengeid);
+    List<Guess> findByIsCorrectTrueAndCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
