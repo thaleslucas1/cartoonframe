9c1ce3be91504f04864ab4032bb12b27aac5e5d8
thales <thaleslucas36@gmail.com>
2025-07-03 18:07:56 -0300

    feat: adição das autoridades em CustomUserDetailsService

--- backend/src/main/java/com/cartoonframe/app/infra/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/cartoonframe/app/infra/security/CustomUserDetailsService.java
@@ -3,12 +3,15 @@ package com.cartoonframe.app.infra.security;
 import com.cartoonframe.app.model.User;
 import com.cartoonframe.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -20,7 +23,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String nomeUsuario) throws UsernameNotFoundException {
         User user = this.repository.findByEmail(nomeUsuario).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
 
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
         /// modificar quando adicionar os roles
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), List.of(authority));
     }
 }
