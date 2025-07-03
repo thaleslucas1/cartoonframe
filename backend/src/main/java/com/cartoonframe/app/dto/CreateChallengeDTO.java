4a38d6cb8f859a3bd0cc221c7ec0c0961b2e8347
thales <thaleslucas36@gmail.com>
2025-07-03 18:03:53 -0300

    feat: criação do dto de criar desafios do admin

---
+++ backend/src/main/java/com/cartoonframe/app/dto/CreateChallengeDTO.java
@@ -0,0 +1,35 @@
+package com.cartoonframe.app.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CreateChallengeDTO {
+
+    private List<String> imageUrls;
+    private String correctAnswer;
+    private LocalDate releaseDate;
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
