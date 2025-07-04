2eac5b4db1dd4303ffa46ba9afa0f19cc09ec440
thales <thaleslucas36@gmail.com>
2025-07-04 20:25:37 -0300

    feat: adição de busca por tentativas certas por data dentro de uma semana no GuessesRepository

--- backend/src/main/java/com/cartoonframe/app/repository/GuessesRepository.java
+++ backend/src/main/java/com/cartoonframe/app/repository/GuessesRepository.java
@@ -1,6 +1,7 @@
 package com.cartoonframe.app.repository;
 
 import com.cartoonframe.app.model.Guess;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
