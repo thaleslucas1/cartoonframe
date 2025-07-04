33b42dd827650f972dd85bf97532e10ce225bf9b
thales <thaleslucas36@gmail.com>
2025-07-04 20:25:06 -0300

    feat: adição de busca por desafios por data dentro de uma semana no ChallengeRepository

--- backend/src/main/java/com/cartoonframe/app/repository/ChallengeRepository.java
+++ backend/src/main/java/com/cartoonframe/app/repository/ChallengeRepository.java
@@ -1,13 +1,16 @@
 package com.cartoonframe.app.repository;
 
 import com.cartoonframe.app.model.Challenge;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
     Optional<Challenge> findByDate(LocalDate date);
+    List<Challenge> findByDateBetweenOrderByDateAsc(LocalDate startDate, LocalDate endDate);
 }
